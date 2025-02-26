9c194b190f346fa8540c9f4cf9e9692deebc49bc
Santicode23 <jtrasvinachavez@gmail.com>
2025-02-26 15:43:29 -0600

    Create refactDhcp.sh

---
+++ refactDhcp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Importar módulos
+source validateDhcp.sh
+source configDhcp.sh
+
+# Leer entradas
+SUBRED=$(get_subnet)
+RANGO_INICIO=$(get_range_start)
+RANGO_FINAL=$(get_range_end)
+SERVER_IP=$(get_server_ip)
+
+# Validar entradas
+validate_ip $SERVER_IP
+
+# Configurar red
+configure_network $SERVER_IP
+
+# Instalar servidor DHCP
+install_dhcp
+
+# Configurar DHCP
+configure_dhcp $SUBRED $RANGO_INICIO $RANGO_FINAL $SERVER_IP
+
+# Reiniciar servicio DHCP
+restart_dhcp
+
+echo "Configuración completada exitosamente."
