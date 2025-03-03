a73d107fcc315f46a4c26be2a256e0e193719192
Santicode23 <144631010+Santicode23@users.noreply.github.com>
2025-03-02 23:06:34 -0600

    Update FtpUbuntu.sh

--- FtpUbuntu.sh
+++ FtpUbuntu.sh
@@ -14,16 +14,16 @@ install_vsftpd() {
     systemctl enable vsftpd
     #Fijar la IP
     echo "network:
-    version: 2
-    renderer: networkd
-    ethernets:
-        enp0s3:
-          dhcp4: true
-        enp0s8:
-          addresses: [192.168.0.10/24]
-          gateway4: 192.168.0.1
-          nameservers:
-          addresses: [8.8.8.8, 1.1.1.1]" | sudo tee /etc/netplan/50-cloud-init.yaml > /dev/null
+        version: 2
+        renderer: networkd
+        ethernets:
+            enp0s3:
+              dhcp4: true
+            enp0s8:
+              addresses: [192.168.0.10/24]
+              gateway4: 192.168.0.1
+              nameservers:
+                  addresses: [8.8.8.8, 1.1.1.1]" | sudo tee /etc/netplan/50-cloud-init.yaml > /dev/null
           
     echo "Fijando la IP"
     sudo netplan apply
