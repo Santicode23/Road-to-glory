1e64678c4e25cbc8527056143d6ec3b144e007e7
Santicode23 <144631010+Santicode23@users.noreply.github.com>
2025-03-05 13:02:03 -0600

    Update FtpUbuntu.sh

--- FtpUbuntu.sh
+++ FtpUbuntu.sh
@@ -6,69 +6,6 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-# Función para cambiar de grupo a un usuario FTP
-cambiar_user_grupo() {
-    local FTP_USER NEW_GROUP
-
-    # Pedir el nombre del usuario
-    echo "Ingrese el nombre del usuario que desea cambiar de grupo:"
-    read FTP_USER
-
-    # Verificar si el usuario existe
-    if ! id "$FTP_USER" &>/dev/null; then
-        echo "Error: El usuario '$FTP_USER' no existe."
-        return 1
-    fi
-
-    # Pedir el nuevo grupo
-    echo "Seleccione el nuevo grupo:"
-    echo "1) Reprobados"
-    echo "2) Recursadores"
-    read opc
-
-    case $opc in
-        1)
-            NEW_GROUP="reprobados"
-            ;;
-        2)
-            NEW_GROUP="recursadores"
-            ;;
-        *)
-            echo "Error: Opción inválida."
-            return 1
-            ;;
-    esac
-
-    GROUPS_DIR="/home/ftp/grupos"
-    USER_DIR="/home/ftp/users/$FTP_USER"
-    CURRENT_GROUP=$(ls "$USER_DIR" | grep -E "reprobados|recursadores")
-
-    if [[ "$CURRENT_GROUP" == "$NEW_GROUP" ]]; then
-        echo "El usuario ya pertenece al grupo "$NEW_GROUP"...."
-        return
-    fi
-
-    if [[ -z "$CURRENT_GROUP" ]]; then
-        echo "No se encontró un grupo asignado en la carpeta del usuario."
-        exit 1
-    fi
-
-    echo "Cambiando de grupo $CURRENT_GROUP a $NEW_GROUP para $FTP_USER..."
-    sudo gpasswd -d $FTP_USER $CURRENT_GROUP
-    sudo usermod -aG $NEW_GROUP $FTP_USER
-    sudo fuser -k "$USER_DIR/$CURRENT_GROUP"
-    # Desmontar la carpeta actual
-    sudo umount "$USER_DIR/$CURRENT_GROUP"
-
-    # Renombrar la carpeta
-    sudo mv "$USER_DIR/$CURRENT_GROUP" "$USER_DIR/$NEW_GROUP"
-
-    # Montar la nueva carpeta del grupo
-    sudo mount --bind "/home/ftp/grupos/$NEW_GROUP" "$USER_DIR/$NEW_GROUP"
-
-    echo "Cambio de grupo completado para $FTP_USER."
-}
-
 # Variables principales
 FTP_ROOT="/home/ftp"
 PUBLIC_DIR="$FTP_ROOT/publica"
@@ -129,12 +66,6 @@ sudo usermod -aG "ftpusers" "$FTP_USER"
 echo "Configurando carpetas para $FTP_USER..."
 sudo mkdir -p "$USERS_DIR/$FTP_USER/publica"
 sudo mkdir -p "$USERS_DIR/$FTP_USER/$FTP_GROUP"
-
-# Llamar a la función para cambiar de grupo si es necesario
-read -p "¿Desea cambiar el grupo de un usuario FTP? (s/n): " RESPUESTA
-if [[ "$RESPUESTA" == "s" || "$RESPUESTA" == "S" ]]; then
-    cambiar_user_grupo
-fi
 
 # Enlazar carpetas con mount --bind
 sudo mkdir -p "$USERS_DIR/$FTP_USER/$FTP_USER"
