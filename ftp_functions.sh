877093c9f8b6a59af646cfc088435ae698d96541
Santicode23 <144631010+Santicode23@users.noreply.github.com>
2025-03-07 01:54:48 -0600

    Update ftp_functions.sh

--- ftp_functions.sh
+++ ftp_functions.sh
@@ -188,11 +188,6 @@ cambiarGrupoUsuario(){
 
         # Eliminar al usuario del grupo anterior
         sudo deluser "$usuario" "$grupoAnterior"
-
-        # Eliminar la carpeta del grupo anterior si aún existe
-        if [[ -d "/home/$usuario/$grupoAnterior" ]]; then
-            sudo rm -rf "/home/$usuario/$grupoAnterior"
-        fi
     fi
 
     # Asignar el usuario al nuevo grupo
@@ -200,6 +195,11 @@ cambiarGrupoUsuario(){
     sudo mkdir -p "/home/$usuario/$nuevoGrupo"
     sudo mount --bind "/home/servidorftp/grupos/$nuevoGrupo" "/home/$usuario/$nuevoGrupo"
     sudo chgrp "$nuevoGrupo" "/home/$usuario/$nuevoGrupo"
+
+    # Restaurar acceso a la carpeta si el usuario regresa a un grupo previo
+    if [[ -d "/home/servidorftp/grupos/$nuevoGrupo" ]]; then
+        echo "Restaurando acceso a los archivos previos del grupo '$nuevoGrupo'..."
+    fi
 
     # Cambiar permisos de las carpetas personales del usuario
     echo "Actualizando permisos de los archivos del usuario..."
