4c3402702a4d589f1998052f9857f05134838692
Santicode23 <jtrasvinachavez@gmail.com>
2025-03-11 15:07:27 -0600

    Create validateDns.ps1

---
+++ validateDns.ps1
@@ -0,0 +1,16 @@
+# validate.ps1
+function Validate-IP {
+    param([string]$IP)
+    if (-not ($IP -match "^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")) {
+        Write-Host "Error: La dirección IP ingresada no es válida." -ForegroundColor Red
+        exit
+    }
+}
+
+function Validate-Domain {
+    param([string]$Dominio)
+    if (-not ($Dominio -match "^(?:[a-zA-Z0-9]+\.)+[a-zA-Z]{2,}$")) {
+        Write-Host "Error: El dominio ingresado no es válido." -ForegroundColor Red
+        exit
+    }
+}
