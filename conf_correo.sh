d2266873f1a602446f5af8d777f4435a4dc74e4d
Santicode23 <144631010+Santicode23@users.noreply.github.com>
2025-04-02 17:45:01 -0600

    Update conf_correo.sh

--- conf_correo.sh
+++ conf_correo.sh
@@ -3,7 +3,7 @@
 
 conf_correo(){
     dominio="elhueso.com"
-    ip="192.168.0.20"
+    ip="20.0.0.10"
     # Actualiza la lista de paquetes 
     sudo apt update 
     sudo apt-get install apache2 -y
