e3c8fc0e7b4c69f36e054d99ed14aac47fc72145
Santicode23 <144631010+Santicode23@users.noreply.github.com>
2025-04-02 22:54:09 -0600

    Update conf_correo.sh

--- conf_correo.sh
+++ conf_correo.sh
@@ -9,7 +9,6 @@ conf_correo(){
     
     sudo apt install software-properties-common -y
     sudo add-apt-repository ppa:ondrej/php -y
-    sudo apt update
     sudo apt install php7.4 libapache2-mod-php7.4 php-mysql -y
 
     # Evitar la pantalla interactiva de configuración
